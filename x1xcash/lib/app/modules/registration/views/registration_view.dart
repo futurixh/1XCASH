9199a46a2aaa5a5586f93b3b3ae0681227b08f43
kilian-v <vitoukilian@gmail.com>
2022-02-08 15:01:11 +0100

    begin

--- x1xcash/lib/app/modules/registration/views/registration_view.dart
+++ x1xcash/lib/app/modules/registration/views/registration_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:x1xcash/app/core/utils/extensions.dart';
 
 import '../controllers/registration_controller.dart';
 
@@ -8,14 +9,42 @@ class RegistrationView extends GetView<RegistrationController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('RegistrationView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'RegistrationView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Text(
+                      "Devenez partenaire",
+                      style: TextStyle(
+                          fontSize: 20.00.sp,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "1XCASH",
+                      style: TextStyle(
+                        fontSize: 20.00.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green
+                      ),
+                    ),
+                    Text(
+                      "Effectuez des opérations de Dépôt et Retrait sur 1XBET à partir de votre compte marchand 1XCASH",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+            ),
+            Expanded(
+                flex: 2,
+                child: SizedBox()
+            )
+          ],
         ),
       ),
     );
