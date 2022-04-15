1d6833fe257fd1c38adf183670c4eb94df2e3a67
kilian-v <vitoukilian@gmail.com>
2022-04-15 11:22:36 +0100

    web project

---
+++ flutter_web_seo_starter/lib/pages/home.view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  static String routeName = '/';
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        children: [
+          Expanded(
+              child: Container(
+                color: Colors.white,
+              )
+          ),
+          Expanded(
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
